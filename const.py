760c97ba3024c66f868de2ef692f083e5c8f9743
Jan Guth <Guth_jan@yahoo.com>
2020-12-29 15:56:10 +0100

    fixed some stuff, added vars, changed var names for better context

--- const.py
+++ const.py
@@ -1,5 +1,10 @@
 plantArray=["AlocasiaZ", "Avocado", "CalathiaM","Monstera"]
 
+plantAccValues = []
+
+init = False
+init_HAP = False
+
 debuglevel = 5
 # 0 none
 # 1 error
@@ -9,3 +14,24 @@ debuglevel = 5
 debugStr = ["None  :  ", "Error :  ", "Notice:  ", "Info  :  ", "Debug :  "]
 
 changedState = False
+
+
+
+
+pubPumpOn = "/Pump/setOn"
+pubEnableWatering = "/enableWatering/setOn"
+pubSetWaterTarget = "/WaterTarget/setValue"
+pubReset = "/Kill"
+
+subLog =                    "/Log"
+subCurrentMoisture =        "/currentMoisture"
+subPumpOn =                 "/Pump/getOn"
+subEnableWatering =         "/enableWatering/getOn"
+subWaterTargetIncrease =    "/WaterTarget/getIncrease"
+subWaterTargetDecrease =    "/WaterTarget/getDecrease"
+subWaterTargetValue =       "/WaterTarget/currentValue"
+subError =                  "/MotionSensor/Error"
+subWatering =               "/MotionSensor/Watering"
+subBattery =                "/Battery"
+subPing =                   "/Ping/Response"
+subFirmware =               "/Firmware"
--- main.py
+++ main.py
@@ -10,29 +10,10 @@ import const
 
 import paho.mqtt.client as mqtt
 
-from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.background import BackgroundScheduler
 
 
 from time import sleep
-
-
-pubPumpOn = "/Pump/setOn"
-pubEnableWatering = "/enableWatering/setOn"
-pubSetWaterTarget = "/WaterTarget/setValue"
-pubReset = "/Kill"
-
-subLog =                    "/Log"
-subCurrentMoisture =        "/currentMoisture"
-subPumpOn =                 "/Pump/getOn"
-subEnableWatering =         "/enableWatering/getOn"
-subWaterTargetIncrease =    "/WaterTarget/getIncrease"
-subWaterTargetDecrease =    "/WaterTarget/getDecrease"
-subWaterTargetValue =       "/WaterTarget/currentValue"
-subError =                  "/MotionSensor/Error"
-subWatering =               "/MotionSensor/Watering"
-subBattery =                "/Battery"
-subPing =                   "/Ping/Response"
-subFirmware =               "/Firmware"
 
 logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
 
@@ -63,25 +44,32 @@ class Plant(Accessory):
     category = CATEGORY_HUMIDIFIER
 
     plantNum = - 1
-    firmwareString = "0.1"
+    plantDic1 = {}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.plantNum = int(self.display_name)
+        self.display_name = str(const.plantArray[self.plantNum]+' Irrigation')
 
         plant_hum = self.add_preload_service("HumidifierDehumidifier",
                                              chars=['RelativeHumidityHumidifierThreshold',
                                                     'RelativeHumidityDehumidifierThreshold',
                                                     'Name'])
+        self.char_name = plant_hum.configure_char('Name')
 
         # passive set via .set_value
         self.char_currentMoisture = plant_hum.configure_char('CurrentRelativeHumidity')
-        self.char_name = plant_hum.configure_char('Name')
+        self.char_currentMoisture.set_value(const.plantAccValues[self.plantNum]["moisture"])
 
         # CurrentHumidifierDehumidifierState: 0,1,2,3
         # (inactive, idle, humidifying, dehumidifying)
 
         self.char_curr_state = plant_hum.configure_char('CurrentHumidifierDehumidifierState')
-        self.char_curr_state.set_value(1)
+        if const.plantAccValues[self.plantNum]["WateringEnabled"]:
+            self.char_curr_state.set_value(1)
+        else:
+            self.char_curr_state.set_value(0)
 
         # active with func
         # TargetHumidifierDehumidifierState
@@ -96,14 +84,12 @@ class Plant(Accessory):
             'Active', setter_callback=self.set_active)
         self.char_threshold = plant_hum.configure_char(
             'RelativeHumidityHumidifierThreshold', setter_callback=self.set_humidity)
+        self.char_threshold.set_value(const.plantAccValues[self.plantNum]["moistureTarget"])
+
         self.char_threshold_de = plant_hum.configure_char(
             'RelativeHumidityDehumidifierThreshold', setter_callback=self.set_humidity_de)
-        self.char_threshold.set_value(60)
         self.char_threshold_de.set_value(100)
-        # self.set_info_service(firmware_revision = self.firmwareString, manufacturer = "JPCG", model = "test")
-
-    def test(self, value):
-        print(value)
+        self.set_info_service(firmware_revision = const.plantAccValues[self.plantNum]['firmware'], manufacturer = "JPCG", model = "Nano")
 
     def set_active(self, value):
         # 0, 1
@@ -112,12 +98,12 @@ class Plant(Accessory):
 
         if value == 0:
             print("Active: is 0, setting enable Watering to false")
-            client.publish(const.plantArray[self.plantNum] + pubPumpOn, "false")
+            client.publish(const.plantArray[self.plantNum] + const.pubPumpOn, "false")
             sleep(2)
-            client.publish(const.plantArray[self.plantNum] + pubEnableWatering, "false")
+            client.publish(const.plantArray[self.plantNum] + const.pubEnableWatering, "false")
         print("Active: is 1, setting enable Watering to true")
         if value == 1:
-            client.publish(const.plantArray[self.plantNum] + pubEnableWatering, "true")
+            client.publish(const.plantArray[self.plantNum] + const.pubEnableWatering, "true")
             self.char_curr_state.set_value(1)
 
 
@@ -125,7 +111,7 @@ class Plant(Accessory):
         log("Set dehum value to " + str(value) + "-> Resetting to 100", 4)
         self.char_threshold_de.set_value(100)
         self.char_threshold.set_value(value)
-        client.publish(const.plantArray[self.plantNum] + pubSetWaterTarget, value)
+        client.publish(const.plantArray[self.plantNum] + const.pubSetWaterTarget, value)
 
     def set_humidity(self, value):
     #     hum_val = value + 10
@@ -133,11 +119,11 @@ class Plant(Accessory):
     #     log("Set dehum_value to " + str(value), 4)
     #     self.char_threshold.set_value(hum_val)
     #     log("Setting hum_value to " + str(hum_val), 4)
-    #     client.publish(const.plantArray[self.plantNum] + pubSetWaterTarget, value)
+    #     client.publish(const.plantArray[self.plantNum] + const.pubSetWaterTarget, value)
         log("Setting hum_value to " + str(value), 4)
         self.char_threshold_de.set_value(100)
         self.char_threshold.set_value(value)
-        client.publish(const.plantArray[self.plantNum] + pubSetWaterTarget, value)
+        client.publish(const.plantArray[self.plantNum] + const.pubSetWaterTarget, value)
 
 
     def set_Target(self, value):
@@ -149,11 +135,11 @@ class Plant(Accessory):
 
         if value == 1:
             log("Start Pump", 4)
-            client.publish(const.plantArray[self.plantNum] + pubPumpOn, "true")
+            client.publish(const.plantArray[self.plantNum] + const.pubPumpOn, "true")
 
         if value == 2:
             log("Selected Dehumidifier = turn off", 4)
-            client.publish(const.plantArray[self.plantNum] + pubEnableWatering, "false")
+            client.publish(const.plantArray[self.plantNum] + const.pubEnableWatering, "false")
             self.char_active.set_value(0)
 
 #
@@ -182,56 +168,65 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     for i in range(len(const.plantArray)):
-        client.subscribe(const.plantArray[i] + subCurrentMoisture)
-        client.subscribe(const.plantArray[i] + subEnableWatering)
-        client.subscribe(const.plantArray[i] + subWaterTargetValue)
-        client.subscribe(const.plantArray[i] + subPumpOn)
-        client.subscribe(const.plantArray[i] + subWatering)
-        client.subscribe(const.plantArray[i] + subFirmware)
-
+        client.subscribe(const.plantArray[i] + const.subCurrentMoisture)
+        client.subscribe(const.plantArray[i] + const.subEnableWatering)
+        client.subscribe(const.plantArray[i] + const.subWaterTargetValue)
+        client.subscribe(const.plantArray[i] + const.subPumpOn)
+        client.subscribe(const.plantArray[i] + const.subWatering)
+        client.subscribe(const.plantArray[i] + const.subFirmware)
 
 def on_message(client, userdata, msg):
+    if const.init:  # disable receiving stray Messages during initial init -> errors
+        messageText = str(msg.payload, 'utf-8')
+        print(msg.topic + " " + str(msg.payload))
+        log(msg.topic + " " + messageText, 4)
+        # CHECK FOR PLANT SPECIFIC MESSAGES
+        for i in range(len(const.plantArray)):
+            if msg.topic == const.plantArray[i]+const.subWatering:
+                if messageText == "true":
+                    # set target state to humidify
+                    if const.init_HAP: # disable reporting while plantAccessories is not yet initialized
+                        plantAccessories[i].char_target_state.set_value(1)
+                        plantAccessories[i].char_curr_state.set_value(2)
+                if messageText == "false":
+                    # set target state to auto
+                    if const.init_HAP:
+                        plantAccessories[i].char_target_state.set_value(0)
+                        plantAccessories[i].char_curr_state.set_value(1)
+            if msg.topic == const.plantArray[i]+const.subPumpOn:
+                if messageText == "true":
+                    # set target state to humidify
+                    if const.init_HAP:
+                        plantAccessories[i].char_target_state.set_value(1)
+                        plantAccessories[i].char_curr_state.set_value(2)
+                if messageText == "false":
+                    if const.init_HAP:
+                    # set target state to auto
+                        plantAccessories[i].char_target_state.set_value(0)
+                        plantAccessories[i].char_curr_state.set_value(1)
+            if msg.topic == const.plantArray[i]+const.subEnableWatering:
+                if messageText == "true":
+                    if const.init_HAP:
+                        plantAccessories[i].char_active.set_value(1)
+                    const.plantAccValues[i]["WateringEnabled"] = True
+                if messageText == "false":
+                    if const.init_HAP:
+                        plantAccessories[i].char_active.set_value(0)
+                    const.plantAccValues[i]["WateringEnabled"] = False
 
-    messageText = str(msg.payload, 'utf-8')
-    print(msg.topic + " " + str(msg.payload))
-    log(msg.topic + " " + messageText, 4)
-    # CHECK FOR PLANT SPECIFIC MESSAGES
-    for i in range(len(const.plantArray)):
-        if msg.topic == const.plantArray[i]+subWatering:
-            if messageText == "true":
-                # set target state to humidify
-                plantAccessories[i].char_target_state.set_value(1)
-                plantAccessories[i].char_curr_state.set_value(2)
-            if messageText == "false":
-                # set target state to auto
-                plantAccessories[i].char_target_state.set_value(0)
-                plantAccessories[i].char_curr_state.set_value(1)
-        if msg.topic == const.plantArray[i]+subPumpOn:
-            if messageText == "true":
-                # set target state to humidify
-                plantAccessories[i].char_target_state.set_value(1)
-                plantAccessories[i].char_curr_state.set_value(2)
-            if messageText == "false":
-                # set target state to auto
-                plantAccessories[i].char_target_state.set_value(0)
-                plantAccessories[i].char_curr_state.set_value(1)
-        if msg.topic == const.plantArray[i]+subEnableWatering:
-            if messageText == "true":
-                plantAccessories[i].char_active.set_value(1)
-            if messageText == "false":
-                plantAccessories[i].char_active.set_value(0)
 
-        if msg.topic == const.plantArray[i]+subCurrentMoisture:
-            plantAccessories[i].char_currentMoisture.set_value(int(messageText))
+            if msg.topic == const.plantArray[i]+const.subCurrentMoisture:
+                if const.init_HAP:
+                    plantAccessories[i].char_currentMoisture.set_value(int(messageText))
+                const.plantAccValues[i]["moisture"] = int(messageText)
 
-        if msg.topic == const.plantArray[i]+subWaterTargetValue:
-            plantAccessories[i].char_threshold.set_value(int(messageText))
+            if msg.topic == const.plantArray[i]+const.subWaterTargetValue:
+                if const.init_HAP:
+                    plantAccessories[i].char_threshold.set_value(int(messageText))
+                const.plantAccValues[i]["moistureTarget"] = int(messageText)
 
-        if msg.topic == const.plantArray[i] + subFirmware:
-            if plantAccessories[i].firmwareString != messageText:
-                plantAccessories[i].firmwareString = messageText
-                plantAccessories[i].set_info_service(firmware_revision = messageText, manufacturer = "JPCG", model = "NanoIoTWaterV1")
-                const.changedState = True
+            if msg.topic == const.plantArray[i] + const.subFirmware:
+                const.plantAccValues[i]["firmware"] = messageText
 
 def checkChangedState(driver):
     if const.changedState:
@@ -248,6 +243,8 @@ def log(text, level):
         client.publish("pyPlant/Log", const.debugStr[level]+text)
 
 
+for plant in const.plantArray:
+    const.plantAccValues.append({"name": plant, "moisture": 40, "moistureTarget": 60, "firmware": "0.1", "WateringEnabled": True})
 
 # sleep(10.0)  # wait for everything to connect (Wifi, etc)
 client = mqtt.Client()
@@ -257,33 +254,38 @@ client.connect("10.0.0.16", 1883, 60)
 client.loop_start()
 sleep(2)
 log("MQTT Started", 2)
+const.init = True
+sleep(2)
+
+for plant in const.plantArray:
+    log("Send /StartUpHAP for " + plant, 2)
+    client.publish(plant+"/StartUpHAP", "Start")
+    sleep(2)
+
 log("Waiting For Everything To Settle", 2)
-sleep(5)
+sleep(10)
 
 
 driver = AccessoryDriver(port=51826, persist_file='busy_home.state')
 bridge = Bridge(driver, 'Bridge')
 plantAccessories = []
-for plant in const.plantArray:
-    plantAccessories.append(Plant(driver, 'Irrigation '+plant))
+for i in range(len(const.plantArray)):
+    plantAccessories.append(Plant(driver, str(i)))
 for i in range(len(const.plantArray)):
     plantAccessories[i].char_active.set_value(1)
     plantAccessories[i].char_name.set_value(const.plantArray[i])
-    plantAccessories[i].plantNum = i
     bridge.add_accessory(plantAccessories[i])
 
-
+const.init_HAP = True
 
 # bridge.add_accessory(FakeFan(driver, 'Big Fan'))
 # bridge.add_accessory(GarageDoor(driver, 'Garage'))
 # bridge.add_accessory(TemperatureSensor(driver, 'Sensor'))
 
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(checkChangedState,  'interval', args=[driver], minutes=1)
+# scheduler = BackgroundScheduler()
+# scheduler.start()
+# scheduler.add_job(checkChangedState,  'interval', args=[driver], minutes=1)
 
 driver.add_accessory(accessory=bridge)
 signal.signal(signal.SIGTERM, driver.signal_handler)
 driver.start()
-
-driver.config_changed()
